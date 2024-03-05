1a5539f22fb22a9679c7330a2c51f08e95f2058d
Lukas <mrspinkyspy@gmail.com>
2024-03-05 14:31:10 +0100

    changed homepage

--- portfolio/src/Pages/Home.js
+++ portfolio/src/Pages/Home.js
@@ -1,6 +1,8 @@
 import Footer from "../Components/Footer/Footer";
 import Navbar from "../Components/Navbar/Navbar";
 import "./Home.css"
+import myPhoto from "../Assets/myPhoto.jpg"
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
     return (
@@ -14,16 +16,30 @@ const Home = () => {
                             <span className="myName">Łukasz Spychała </span>
                             <span className="softDev">Software Developer</span>
                         </div>
+                        <div className="myPhotoDiv">
+                            <img src={myPhoto} alt="myPhoto" className="myPhoto"></img>
+                        </div>
                     </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ultricies nisl nec convallis dapibus. Praesent et tellus ut mi finibus placerat. Etiam sodales maximus ante, ac viverra libero porta ac. Proin accumsan posuere lobortis. Pellentesque lacinia rutrum condimentum. Phasellus vitae tincidunt justo, non faucibus nibh. Morbi scelerisque felis consectetur, interdum quam ut, pretium sapien.
-                       Suspendisse at ultricies augue. Sed varius lacus sit amet tortor auctor viverra. Nunc at sagittis dui, a tempus ante. Integer in sagittis lacus, sit amet egestas arcu. Vivamus in eros odio. Sed scelerisque urna ut pharetra varius. In non pellentesque diam, ut consequat augue. Donec ultrices congue augue, et consectetur urna fermentum sed. Duis sollicitudin egestas augue sed dapibus. Mauris eleifend massa leo, sit amet mattis ex tristique sed. In feugiat gravida ipsum. Quisque in fermentum erat. Nulla porttitor felis nec neque interdum, at ultrices sem venenatis. Donec ut tellus interdum, luctus massa vel, rutrum mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ultricies nisl nec convallis dapibus. Praesent et tellus ut mi finibus placerat. Etiam sodales maximus ante, ac viverra libero porta ac. Proin accumsan posuere lobortis. Pellentesque lacinia rutrum condimentum. Phasellus vitae tincidunt justo, non faucibus nibh. Morbi scelerisque felis consectetur, interdum quam ut, pretium sapien.
-                       Suspendisse at ultricies augue. Sed varius lacus sit amet tortor auctor viverra. Nunc at sagittis dui, a tempus ante. Integer in sagittis lacus, sit amet egestas arcu. Vivamus in eros odio. Sed scelerisque urna ut pharetra varius. In non pellentesque diam, ut consequat augue. Donec ultrices congue augue, et consectetur urna fermentum sed. Duis sollicitudin egestas augue sed dapibus. Mauris eleifend massa leo, sit amet mattis ex tristique sed. In feugiat gravida ipsum. Quisque in fermentum erat. Nulla porttitor felis nec neque interdum, at ultrices sem venenatis. Donec ut tellus interdum, luctus massa vel, rutrum mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-                       Suspendisse at ultricies augue. Sed varius lacus sit amet tortor auctor viverra. Nunc at sagittis dui, a tempus ante. Integer in sagittis lacus, sit amet egestas arcu. Vivamus in eros odio. Sed scelerisque urna ut pharetra varius. In non pellentesque diam, ut consequat augue. Donec ultrices congue augue, et consectetur urna fermentum sed. Duis sollicitudin egestas augue sed dapibus. Mauris eleifend massa leo, sit amet mattis ex tristique sed. In feugiat gravida ipsum. Quisque in fermentum erat. Nulla porttitor felis nec neque interdum, at ultrices sem venenatis. Donec ut tellus interdum, luctus massa vel, rutrum mi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
+
+                    <div className="linksDiv">
+                        <NavLink to="/projects" className="projectsLink">
+                            <img src={myPhoto} alt="projectsPhoto" className="projectsPhoto"></img>
+                            <span className="projectsLinkText">Check my personal projects here...</span>
+                        </NavLink>
+                        <div className="aboutMeLink">
+                            <span>Learn more about me</span>
+                            <img src={myPhoto} alt="aboutMePhoto" className="aboutMePhoto"></img>
+                        </div>
+                        <div className="hobbiesLink">
+                            <img src={myPhoto} alt="hobbiesPhoto" className="hobbiessPhoto"></img>
+                            <span>I do other stuff as well.</span>
+                        </div>
+                    </div>
                 </div>
             </div>
             <Footer />
         </div>
-            )
+    )
 }
 
 export default Home;
