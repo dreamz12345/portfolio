1a1a4714deb64bf1b6dec89ca3e68891534b25db
dreamz <126981331+dreamz12345@users.noreply.github.com>
2024-03-18 14:34:05 +0100

    add arduino

--- portfolio/src/Pages/Projects.js
+++ portfolio/src/Pages/Projects.js
@@ -3,8 +3,10 @@ import Navbar from "../Components/Navbar/Navbar";
 import "./Projects.css"
 import pongSquare from "../Assets/pong_square_size.jpg";
 import pythonLogo from "../Assets/python-logo-only.png";
+import arduinoLogo from "../Assets/arduino-logo.png";
+import cplusplusLogo from "../Assets/c++-logo.png";
 
-import renderPhoto from "../Assets/TT-SU2_700_samples_transparen_background.png";
+import placeholderPhoto from "../Assets/myPhoto.jpg";
 import { NavLink } from "react-router-dom";
 
 const Projects = () => {
@@ -26,6 +28,23 @@ const Projects = () => {
                                 <span className="projectsLinkTextRightMedium">Famous oldschool game written by me in Python Turtle.</span>
                                 <div className="projectsTechnologiesUsedDiv">
                                     <img src={pythonLogo} alt="projectsPhoto" className="technologiesUsedPhoto"></img>
+                                </div>
+                            </div>
+                        </NavLink>
+                    </div>
+
+
+                    <div className="projectsLinksDiv">
+                        <NavLink to="/projects" className="projectsLink">
+                            <div className="projectsPhotoDivRight">
+                                <img src={placeholderPhoto} alt="projectsPhoto" className="projectsPhoto"></img>
+                            </div>
+                            <div className="projectsLinkTextDivRight">
+                                <span className="projectsLinkTextRight">Arduino Heater</span>
+                                <span className="projectsLinkTextRightMedium">Personal project created to control heater pump with Arduino.</span>
+                                <div className="projectsTechnologiesUsedDiv">
+                                    <img src={cplusplusLogo} alt="projectsPhoto" className="cplusplusPhoto"></img>
+                                    <img src={arduinoLogo} alt="projectsPhoto" className="arduinoPhoto"></img>
                                 </div>
                             </div>
                         </NavLink>
