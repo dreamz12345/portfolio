0608f8eee739d88e6c72ed8a7701a02dbda5e4fa
Lukas <mrspinkyspy@gmail.com>
2024-07-01 16:13:44 +0200

    homepage

--- portfolio/src/Pages/Home.js
+++ portfolio/src/Pages/Home.js
@@ -1,11 +1,11 @@
 import Footer from "../Components/Footer/Footer";
 import Navbar from "../Components/Navbar/Navbar";
 import ScrollToTop from "../Components/scrollToTop";
-import "./Home.css"
-import "./Projects.css"
-import myPhoto from "../Assets/placeholder_portrait.png"
-import pongSquare from "../Assets/pong_square_size.jpg"
-import renderPhoto from "../Assets/TT-SU2_700_samples_transparen_background.png"
+import "./Home.css";
+import "./Projects.css";
+import myPhoto from "../Assets/placeholder_portrait.png";
+import pongSquare from "../Assets/pong_square_size.jpg";
+import renderPhoto from "../Assets/TT-SU2_700_samples_transparen_background.png";
 import { NavLink } from "react-router-dom";
 
 const Home = () => {
@@ -21,37 +21,61 @@ const Home = () => {
                         <div className="aboutTopDiv">
                             <img className="aboutPortraitPhoto" alt="My Portrait" src={myPhoto}></img>
                                 <div className="aboutTopText">
-                                    <span className="aboutThinText">My name is <span className="aboutBoldText">Łukasz Spychała.</span></span>
-                                    <span className="aboutThinText">I'm aspiring <span className="aboutBoldText">software developer</span></span>
-                                    <span className="aboutThinText">you can <span className="aboutBoldText">click here</span></span>
-                                    <span className="aboutThinText">to learn more <span className="aboutBoldText">About Me</span></span>
+                                    <span className="thinTxt">My name is <span className="boldTxt">Łukasz Spychała.</span></span>
+                                    <span className="thinTxt">I'm aspiring <span className="boldTxt">software developer</span></span>
+                                    <span className="thinTxt">with a goal of delivering <span className="boldTxt">high quality code</span></span>
+                                    <span className="thinTxt">with <span className="boldTxt">full documentation.</span></span>
                                 </div>
                             </div>
                         </NavLink>
                     </div>
 
-                    <div className="homeLinksDiv">
-                        <NavLink to="/projects" className="homeLink">
-                            <div className="projectsPhotoDivRight">
-                                <img src={pongSquare} alt="projectsPhoto" className="pongPhoto"></img>
-                            </div>
-                            <div className="homeLinkTextDivRight">
-                                <span className="homeLinkTextRight">Check my personal projects here</span>
-                            </div>
-                        </NavLink>
-                    </div>
+                    
+                        <div className="homeDivOuter">
 
-                    <div className="homeLinksDiv">
-                        <NavLink to="/Hobbies" className="homeLink">
-                            <div className="homeLinkTextDivLeft">
-                                <span className="homeLinkTextLeft">or see unreleated stuff like 3D renders.</span>
+                            <div className="homeDivMid">
+                                <NavLink to="/projects">
+                                    <div className="homeDivInner">
+                                        <span className="boldTxt">Check my personal projects here</span>
+                                        <img src={pongSquare} alt="projectsPhoto" className="homePhoto"></img>
+                                    </div>
+                                </NavLink>
                             </div>
-                            <div className="homePhotoDivLeft">
-                                <img src={renderPhoto} alt="homePhoto" className="homePhoto"></img>
-                            </div>
-                        </NavLink>
-                    </div>
 
+                            <div className="homeDivMid">
+                                <NavLink to="/projects">
+                                    <div className="homeDivInner">
+                                        <span className="boldTxt">Check my personal projects here</span>
+                                        <img src={pongSquare} alt="projectsPhoto" className="homePhoto"></img>
+                                    </div>
+                                </NavLink>
+                            </div>
+
+                            <div className="homeDivMid">
+                                <NavLink to="/projects">
+                                    <div className="homeDivInner">
+                                        <span className="boldTxt">Projects</span>
+                                        <img src={pongSquare} alt="projectsPhoto" className="homePhoto"></img>
+                                    </div>
+                                </NavLink>
+                            </div>
+
+                            <div className="homeDivMid">
+                                <NavLink to="/projects">
+                                    <div className="homeDivInner">
+                                        <span className="boldTxt">Hobbies</span>
+                                        <img src={renderPhoto} alt="projectsPhoto" className="homePhoto"></img>
+                                    </div>
+                                </NavLink>
+                            </div>
+
+
+
+                        </div>
+
+
+
+  
 
                 </div>
             </div>
