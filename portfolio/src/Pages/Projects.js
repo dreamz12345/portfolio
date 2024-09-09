ddeeaad24d5a49045acd185580c2a099b760215b
Lukas <lukasz@spychala.dev>
2024-09-09 15:51:42 +0200

    working on projects page

--- portfolio/src/Pages/Projects.js
+++ portfolio/src/Pages/Projects.js
@@ -1,12 +1,14 @@
 import Footer from "../Components/Footer/Footer";
 import Navbar from "../Components/Navbar/Navbar";
 import ScrollToTop from "../Components/scrollToTop";
-import "./Projects.css"
+import "./Projects.css";
+import "./About.css";
 import pongSquare from "../Assets/pong_square_size.jpg";
 import pythonLogo from "../Assets/python-logo-only.png";
 import arduinoLogo from "../Assets/arduino-logo.png";
 import cplusplusLogo from "../Assets/c++-logo.png";
 import GitHubLogo from "../Assets/GitHub_Logo_White.png"
+import pongVideo from "../Assets/pong_animation0000-0120.mp4"
 
 import placeholderPhoto from "../Assets/myPhoto.jpg";
 import { NavLink } from "react-router-dom";
@@ -24,6 +26,61 @@ const Projects = () => {
             <div className="pageAboveFooter">
                 <Navbar />
                 <div className="pageContent">
+
+                <div className="aboutMidDiv">
+                        <span className="aboutTitleText">Projects</span>
+
+                    </div>
+
+                <div className="homeDivOuter">
+
+                <div className="homeDivMid">
+
+                                    <a className="homeDivInner" href={github_link}>
+                                        <span className="boldTxt">All of my Projects</span>
+                                        <img src={GitHubLogo} alt="projectsPhoto" className="homePhotoFit"></img>
+                              
+                                    </a>
+
+                            </div>
+
+                            <div className="homeDivMid">
+                                <NavLink to="/projects">
+                                    <div className="homeDivInner">
+                                        <div className="projectsTechnologiesUsed">
+                                        <span className="boldTxt">Pong</span>
+                                        <img src={pythonLogo} alt="projectsPhoto" className="technologiesUsedPhoto"></img>
+                                        </div>
+                                        <video src={pongVideo} alt="projectsPhoto" className="homePhoto" loop autoPlay muted />
+                                    </div>
+                                </NavLink>
+                            </div>
+
+                            <div className="homeDivMid">
+                                <NavLink to="/hobbies">
+                                    <div className="homeDivInner">
+                                        <span className="boldTxt">Hobbies</span>
+                                        <img src={pongVideo} alt="projectsPhoto" className="homePhoto"></img>
+                                    </div>
+                                </NavLink>
+                            </div>
+
+
+                            <div className="homeDivMid">
+                                <NavLink to="/projects">
+                                    <div className="homeDivInner">
+                                        <span className="boldTxt">Contact</span>
+                                        <video src={pongVideo} alt="projectsPhoto" className="homePhoto" video loop autoPlay muted />
+                                    </div>
+                                </NavLink>
+                            </div>
+
+
+
+
+</div>
+
+
 
                 <div className="projectsLinksDiv">
                         <a href={github_link} className="projectsLink">
